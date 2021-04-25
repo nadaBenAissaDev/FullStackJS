a4f8ef673b844e319a1fe86250b5bcc4cdca77b3
Imen EL HAKIM <imen.elhakim@esprit.tn>
2021-04-25 17:55:23 +0200

    Some Changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+<<<<<<< Updated upstream
+=======
+// import './frontOffice/styles/styles.scss';
+>>>>>>> Stashed changes
 // import React from 'react';
 // import ReactDOM from 'react-dom';
 // import 'bootstrap/dist/css/bootstrap.min.css';
@@ -6,15 +10,19 @@
 // import 'bootstrap/dist/js/bootstrap.bundle.min';
 // import './index.css';
 // import App from './App';
+<<<<<<< Updated upstream
 // import reportWebVitals from './reportWebVitals';
 
 // import './frontOffice/styles/styles.scss';
 
+=======
+>>>>>>> Stashed changes
 // ReactDOM.render(
 //   <React.StrictMode>
 //     <App />
 //   </React.StrictMode>,
 //   document.getElementById('root')
+<<<<<<< Updated upstream
 // );
 
 // reportWebVitals();
@@ -23,6 +31,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+=======
+// ); 
+
+
+ import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+//redux
+import {Provider} from 'react-redux';
+import store from './store';
+>>>>>>> Stashed changes
 
  import "backend/assets/plugins/nucleo/css/nucleo.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
@@ -31,14 +50,26 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
  import AdminLayout from "backend/layouts/Admin.js";
  import AuthLayout from "backend/layouts/Auth.js";
 
+<<<<<<< Updated upstream
 
  ReactDOM.render(
+=======
+ ReactDOM.render(
+  <Provider  store={store}>
+>>>>>>> Stashed changes
   <BrowserRouter>
     <Switch>
       <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
       <Route path="/auth" render={(props) => <AuthLayout {...props} />} />
       <Redirect from="/" to="/admin/index" /> 
     </Switch>  
+<<<<<<< Updated upstream
    </BrowserRouter>,                   
    document.getElementById("root")
  );
+=======
+   </BrowserRouter>
+   </Provider>,                   
+   document.getElementById("root")
+ ); 
+>>>>>>> Stashed changes
