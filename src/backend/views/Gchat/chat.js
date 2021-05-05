63b1842ae1acc3e9c429c7378e3e40b361e87513
ichraksalhi <ichraks@yahoo.com>
2021-05-05 02:53:27 +0100

    modification du chat

--- src/backend/views/Gchat/chat.js
+++ src/backend/views/Gchat/chat.js
@@ -9,15 +9,7 @@ const projectID = 'f27ddfad-772b-4745-a7eb-55affda74429';
 const chat = () => {
   if (!localStorage.getItem('username')) return <LoginForm />;
 
-  return (
-    <ChatEngine
-      height="100vh"
-      projectID={projectID}
-      userName={localStorage.getItem('username')}
-      userSecret={localStorage.getItem('password')}
-      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
-      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
-    />
+ 
   );
 };
 
